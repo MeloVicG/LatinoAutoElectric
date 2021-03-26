bd8319462923ccf7abfb74f792199d1197730dae
Spencer Huyck <huycksl@gmail.com>
2021-03-26 16:35:48 -0700

    added another practice

--- paras-repair-shop/src/views/AppointmentPage.jsx
+++ paras-repair-shop/src/views/AppointmentPage.jsx
@@ -7,6 +7,7 @@ const AppointmentPage = () => {
         <div>
             <h1>Schedule Appointment</h1>
             <h1>PRACTICE</h1>
+            <h1>Let's understand this</h1>
         </div>
 
     )
