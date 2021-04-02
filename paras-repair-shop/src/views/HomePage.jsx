0b509aabb9d78bc7a2a3277a68e717a932a8d07f
MeloVicG <Melovicg41@gmail.com>
2021-04-01 22:15:56 -0700

    first commit again

--- paras-repair-shop/src/views/HomePage.jsx
+++ paras-repair-shop/src/views/HomePage.jsx
@@ -8,7 +8,7 @@ import mapPlaceholder from '../static/images/map-placeholder.PNG';
 
 const HomePage = () => {
 
-    return(
+    return (
         <div className="App">
             <h1>*************************************</h1>
             <div className="wrapper">
@@ -16,36 +16,36 @@ const HomePage = () => {
 
 
                 {/* has css */}
-                <NavBar/>
+                <NavBar />
                 <div className="container">
                     <div className="container-top">
                         <div className="homeTopSection">
-                            <img className="image1" src="https://www.wardsauto.com/sites/wardsauto.com/files/styles/article_featured_retina/public/uploads/2017/03/ford-focus-michigan-assembly-plant.jpg?itok=da7Zvhu9" alt="first image"/>
-                            <img className="image2" src="https://i.pinimg.com/originals/c1/9f/a5/c19fa5a2b6de80d4b5e8866f2cf36c0f.jpg" alt="second"/>
+                            <img className="image1" src="https://www.wardsauto.com/sites/wardsauto.com/files/styles/article_featured_retina/public/uploads/2017/03/ford-focus-michigan-assembly-plant.jpg?itok=da7Zvhu9" alt="first image" />
+                            <img className="image2" src="https://i.pinimg.com/originals/c1/9f/a5/c19fa5a2b6de80d4b5e8866f2cf36c0f.jpg" alt="second" />
                         </div>
                     </div>
                     <div className="quick-schedule">
                         <Link to="/schedule"><button><h1>Schedule Now!</h1></button></Link>
                     </div>
-          
-                <div className="homeMidBox">
+
+                    <div className="homeMidBox">
                         <div >
                             <p className="homeMidP"> Gallery </p>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, culpa ad saepe necessitatibus quidem mollitia numquam nihil eos sunt sequi adipisci, quae est, eaque sapiente. Sit reprehenderit animi voluptate molestias?</p>
-                            <button style={{boxShadow:"1px 1px 1px 1px", backgroundColor:"rgb(18,151,210)", fontSize:"20px"}}>View</button>
+                            {/* <button style={{ boxShadow: "1px 1px 1px 1px", backgroundColor: "white", fontSize: "20px" }}>View</button> */}
                         </div>
                         <div >
                             <p className="homeMidP">Service</p>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, culpa ad saepe necessitatibus quidem mollitia numquam nihil eos sunt sequi adipisci, quae est, eaque sapiente. Sit reprehenderit animi voluptate molestias?</p>
-                            <button style={{boxShadow:"1px 1px 1px 1px", backgroundColor:"rgb(18,151,210)", fontSize:"20px"}}>View</button>
+                            {/* <button style={{ boxShadow: "1px 1px 1px 1px", backgroundColor: "white", fontSize: "20px" }}>View</button> */}
                         </div>
                         <div >
                             <p className="homeMidP"> Customer Reviews </p>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, culpa ad saepe necessitatibus quidem mollitia numquam nihil eos sunt sequi adipisci, quae est, eaque sapiente. Sit reprehenderit animi voluptate molestias?</p>
-                            <button style={{boxShadow:"1px 1px 1px 1px", backgroundColor:"rgb(18,151,210)", fontSize:"20px"}}>View</button>
+                            {/* <button style={{ boxShadow: "1px 1px 1px 1px", backgroundColor: "white", fontSize: "20px" }}>View</button> */}
                         </div>
                     </div>
-      
+
                 </div>
                 <div className="quote">
                     <p>"Latinos Auto Electric has been taking care of my cars for over 20 years. They continue to do an amazing job and I will always choose them to take care of any auto issues I have."</p>
@@ -57,8 +57,8 @@ const HomePage = () => {
                         <Link to="/directions">Get Directions</Link>
                     </div>
                 </div>
-      
-              <footer style={{backgroundColor:"rgb(18,151,210)"}}>
+
+                <footer style={{ backgroundColor: "rgb(18,151,210)" }}>
                     <div>
                         <p>Latinos Auto Electric</p>
                         <p>Contact:</p>
@@ -67,29 +67,29 @@ const HomePage = () => {
                         <p>90813</p>
                         <p>562-218-8560</p>
                     </div>
-                    <div style={{margin:"30px"}}>
+                    <div style={{ margin: "30px" }}>
                         <a href="">something</a>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
+                        <a href="">something</a>
+                        <a href="">something</a>
+                        <a href="">something</a>
                     </div>
-                    <div style={{margin:"30px"}}>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
+                    <div style={{ margin: "30px" }}>
+                        <a href="">something</a>
+                        <a href="">something</a>
+                        <a href="">something</a>
+                        <a href="">something</a>
                     </div>
-                    <div style={{margin:"30px"}}>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
+                    <div style={{ margin: "30px" }}>
+                        <a href="">something</a>
+                        <a href="">something</a>
+                        <a href="">something</a>
+                        <a href="">something</a>
                     </div>
-                    <div style={{margin:"30px"}}>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
-                        <a  href="">something</a>
+                    <div style={{ margin: "30px" }}>
+                        <a href="">something</a>
+                        <a href="">something</a>
+                        <a href="">something</a>
+                        <a href="">something</a>
                     </div>
                 </footer>
             </div>
